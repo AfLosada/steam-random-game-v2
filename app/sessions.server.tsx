2b2db65f292a7b4cca1b3a026c258737dbdd7a5a
Andrés Felipe Losada luna <nedrocoli@gmail.com>
2024-05-31 00:17:24 -0500

    implementing an oidc client on my remix app

--- app/sessions.server.tsx
+++ app/sessions.server.tsx
@@ -1,10 +1,45 @@
 import { createCookieSessionStorage } from "@remix-run/node"
 import { createThemeSessionResolver } from "remix-themes"
 
+
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+//https://aarongodin.dev/blog/adding-oauth-oidc-to-a-remix-app
+import { Issuer } from "openid-client"
+import type { Client } from "openid-client"
+
+let client: Client
+
+async function getClient(): Promise<Client> {
+  if (client !== undefined) {
+    return client
+  }
+  
+  const issuer = await Issuer.discover(config.oidcIssuer)
+  client = new issuer.Client({
+    client_id: config.oidcClientID,
+    client_secret: config.oidcClientSecret,
+    redirect_uris: [`${config.oidcRedirectBase}/auth/callback`],
+    response_types: ['code'],
+  })
+  return client
+}
+
 // You can default to 'development' if process.env.NODE_ENV is not set
 const isProduction = process.env.NODE_ENV === "production"
 
-const sessionStorage = createCookieSessionStorage({
+export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "theme",
     path: "/",

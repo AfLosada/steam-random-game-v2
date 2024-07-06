406d7e24654651e9b389001a97c919697aaab4b4
Andrés Felipe Losada luna <nedrocoli@gmail.com>
2024-07-06 15:46:36 -0500

    fixes localhost routing

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -9,7 +9,7 @@ export const authenticator = new Authenticator<User>(sessionStorage);
 authenticator.use(
   new SteamStrategy(
     {
-      returnURL: "http://localhost:5173/auth/steam/callback",
+      returnURL: "/auth/steam/callback",
       apiKey: process.env.STEAM_KEY || "", // you can get it here: https://steamcommunity.com/dev/apikey
     },
     async (user) => user // perform additional checks for user here, I just leave this to SteamStrategyVerifyParams value
